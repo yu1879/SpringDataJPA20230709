fada6c192becf84f16906bc971cf4040fc76eeec
yu1879 <shengweiyukobe0526@gmail.com>
2023-07-16 13:20:56 +0800

    jpa

--- src/main/java/com/spring/mvc/single/controller/PersonController.java
+++ src/main/java/com/spring/mvc/single/controller/PersonController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -23,6 +24,17 @@ public class PersonController {
 	public String index(@ModelAttribute Person person, Model model) {
 		List<Person> persons = personRepository.findAll();
 		model.addAttribute("_method", "POST");
+		model.addAttribute("persons", persons);
+		return "person/index";
+
+	}
+
+	@GetMapping("/{id}")
+	public String getPersonById(@PathVariable Long id, Model model) {
+		Person person = personRepository.findOne(id);
+		List<Person> persons = personRepository.findAll();
+		model.addAttribute("person", person);
+		model.addAttribute("_method", "PUT");
 		model.addAttribute("persons", persons);
 		return "person/index";
 
